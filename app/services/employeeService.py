c8bdae172e23cf3ee9d71e25f0dec7c3421a24b3
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-03-08 14:02:07 -0300

    feat: employee service prototype created

---
+++ app/services/employeeService.py
@@ -0,0 +1,39 @@
+import json
+from fastapi import Response
+from app.database import get_db
+from app.models.EmployeeModel import Employee
+
+class EmployeeService:
+    def __init__():
+        pass
+
+    def list_all_employees(self):
+        db = get_db()
+        data = db.query(Employee).all()
+
+        return {"data": data}
+
+    def view_one_employee(self, id):
+        db = get_db()
+        data = db.query(Employee).get(id)
+
+        return {"data": data}
+    
+    def create_one_employee(self, request):
+        db = get_db()
+        email = request.get("email").lower()
+
+        if db.query(Employee).filter(Employee.email == email).first():
+            return Response(json.dumps({"error": True, "message": "email already exists"}), 409)
+        
+        newEmployee = Employee(**request)
+        db.add(newEmployee)
+
+        try:
+            db.flush()
+            db.commit()
+            return Response(json.dumps({"error": False, "message": "Employee created successfully"}), 200)
+        except:
+            db.rollback()
+            return Response(json.dumps({"error": True, "message": "Database error"}), 500)
+ 
