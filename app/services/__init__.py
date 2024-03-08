9e0282054c7288eb90e0a7f17da1525d3be581f6
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-03-08 15:31:50 -0300

    style: indentation fix

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,23 +1,23 @@
 def instance_update(instance, request_json):
-    """
-    This function updates every key received from the request in an instance of a table, if the key exists in that table.
-    
-    Example: The table User has three columns (id, name, email) and the request object has five fields (name, age, bloodType, email, address).
-    The updated fields in the instance will be (name, email).
+  """
+  This function updates every key received from the request in an instance of a table, if the key exists in that table.
+  
+  Example: The table User has three columns (id, name, email) and the request object has five fields (name, age, bloodType, email, address).
+  The updated fields in the instance will be (name, email).
 
-    The parameter instance should be a query from a table. 
+  The parameter instance should be a query from a table. 
 
-    `instance = User.query.get(id)`
-    """
+  `instance = User.query.get(id)`
+  """
 
-    instance_keys: list[str] = list(instance.to_dict().keys())
+  instance_keys: list[str] = list(instance.to_dict().keys())
 
-    for key in instance_keys:
-      if key in request_json and request_json[key] is not None:
-        setattr(instance, key, request_json.get(key))
-    
-    if request_json.get("email") is not None:
-      setattr(instance, 'email', request_json.get("email").lower())
-    
-    #if request_json.get("password") is not None:
-    #  setattr(instance, 'password', bcrypt_context.hash(request_json.get("password")))
+  for key in instance_keys:
+    if key in request_json and request_json[key] is not None:
+      setattr(instance, key, request_json.get(key))
+  
+  if request_json.get("email") is not None:
+    setattr(instance, 'email', request_json.get("email").lower())
+  
+  #if request_json.get("password") is not None:
+  #  setattr(instance, 'password', bcrypt_context.hash(request_json.get("password")))
