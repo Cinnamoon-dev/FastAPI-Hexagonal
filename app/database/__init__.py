63fad91587789067094868abddfaf2bd20e28296
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-04-29 13:58:26 -0300

    feat: adding env variable for db conn

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("postgresql://postgres:1234@localhost:5432/hexagonal")
+load_dotenv()
+
+engine = create_engine(os.getenv('DATABASE_URI', None))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
