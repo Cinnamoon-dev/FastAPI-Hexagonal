347a3a2791d261c42d1f87128bc96da8d5a4f5d3
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-04-29 13:47:59 -0300

    refactor: changing uvicorn run
    Running the application with OTEL needs the option reload to be False.
    So this option was disabled and the api will run in all the hosts in the
    network.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
+from fastapi import FastAPI
 from app.database import Base, engine
 from app.controllers import employeeRouter, vehicleRouter
-from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -10,4 +10,4 @@ app.include_router(vehicleRouter.router)
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', host='0.0.0.0')
