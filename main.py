1eea194ce43fb1b1c36420c4c93d1dd8de80ab5c
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-05-09 21:31:11 -0300

    feat: adding instrumentator
    Testing if instrumentator is needed in the final implementation

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.controllers import employeeRouter, vehicleRouter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
 app = FastAPI()
 
 app.include_router(employeeRouter.router)
 app.include_router(vehicleRouter.router)
+
+FastAPIInstrumentor(app)
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
