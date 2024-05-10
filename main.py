7c494c695d0003225ad1dc0af20005839d4e8e5f
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-05-09 21:33:11 -0300

    style: organizing imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
+from fastapi import FastAPI
 from app.database import Base, engine
 from app.controllers import employeeRouter, vehicleRouter
-from fastapi import FastAPI
 
 app = FastAPI()
 
