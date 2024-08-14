e16f8511c8ca19702ff3285f17e54baa72946c6f
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-08-14 11:02:36 -0300

    refactor: changing Column to mapped_column

--- app/models/EmployeeModel.py
+++ app/models/EmployeeModel.py
@@ -1,26 +1,25 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import String
 from app.models.VehicleModel import Vehicle
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Employee(Base):
     __tablename__ = "employee"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    email = Column(String(255), unique=True, nullable=False)
-    password = Column(String, nullable=False)
-    wage = Column(Float, nullable=False)
-    
-    vehicles = relationship(Vehicle, back_populates='employee')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    wage: Mapped[float] = mapped_column(nullable=False)
+
+    vehicles: Mapped[list[Vehicle]] = relationship(Vehicle, back_populates='employee')
 
     def __init__(self, name, email, password, wage):
         self.name = name
         self.email = email
         self.password = password
         self.wage = wage
-
+    
     def to_dict(self):
         data = {
             "id": self.id,
@@ -33,9 +32,9 @@ class Employee(Base):
 
         for i in self.vehicles:
             data["vehicles"].append(i.relationship_to_dict())
-
+        
         return data
-
+    
     def relationship_to_dict(self):
         data = {
             "id": self.id,
