c317facfe68720a5a437b50b28c79a738a35fe82
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-09-02 14:01:17 -0300

    feat: adding auth router and port env variable

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import uvicorn
+import os, uvicorn
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.controllers import employeeRouter, vehicleRouter
+from app.controllers import employeeRouter, vehicleRouter, authRouter
+
 
 app = FastAPI()
 
 app.include_router(employeeRouter.router)
 app.include_router(vehicleRouter.router)
+app.include_router(authRouter.router)
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
